5c810ac6cd70bc4a034401dd4f3e152faaf0710e
Ksenia <KVLushanina@stud.kpfu.ru>
2025-06-17 22:16:00 +0300

    отредактирован фронтенд входа и регистрации

---
+++ Neveroyatno/Migrations/20250617140930_SyncAll.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Neveroyatno.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncAll : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Lectures",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Lectures",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}

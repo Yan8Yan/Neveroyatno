5ad18b9b45c1f3b7fb0e1087dc577654501f8047
Anna <ahha271104@mail.ru>
2025-06-18 03:45:23 +0300

    Добавлена статистика

---
+++ Neveroyatno/Migrations/20250618001048_AddTestResult.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Neveroyatno.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTestResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TestId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    Total = table.Column<int>(type: "integer", nullable: false),
+                    Percentage = table.Column<int>(type: "integer", nullable: false),
+                    PassedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CertificateFileName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestResults_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TestResults_Tests_TestId",
+                        column: x => x.TestId,
+                        principalTable: "Tests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestResults_TestId",
+                table: "TestResults",
+                column: "TestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestResults_UserId",
+                table: "TestResults",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestResults");
+        }
+    }
+}
---
+++ Neveroyatno/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Neveroyatno.Models;
+
+namespace Neveroyatno.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public int CompletedTests { get; set; }
+        public int TotalTests { get; set; }
+
+        public List<TestResult> Results { get; set; }
+    }
+
+
+}
